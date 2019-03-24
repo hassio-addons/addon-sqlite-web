96ca892ed7578fcd3e54acf5d477b857f91ef862
ludeeus <joasoe@gmail.com>
2019-03-24 12:04:07 +0100

    🔨 Rewrite to use bashio

--- sqlite-web/rootfs/etc/cont-init.d/10-requirements.sh
+++ sqlite-web/rootfs/etc/cont-init.d/10-requirements.sh
@@ -1,56 +1,25 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: SQLite Web
 # This files check if all user configuration requirements are met
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
 
 # Require username / password
-if ! hass.config.has_value 'username' \
-    && ! ( \
-        hass.config.exists 'leave_front_door_open' \
-        && hass.config.true 'leave_front_door_open' \
-    );
-then
-    hass.die 'You need to set a username!'
-fi
-
-if ! hass.config.has_value 'password' \
-    && ! ( \
-        hass.config.exists 'leave_front_door_open' \
-        && hass.config.true 'leave_front_door_open' \
-    );
-then
-    hass.die 'You need to set a password!';
+if ! bashio::config.true 'leave_front_door_open'; then
+    bashio::config.require.username 'username';
+    bashio::config.require.password 'password';
 fi
 
 # Require a secure password
-if hass.config.has_value 'password' \
-    && ! hass.config.is_safe_password 'password'; then
-    hass.die "Please choose a different password, this one is unsafe!"
+if bashio::config.has_value 'password' \
+    && ! bashio::config.true 'i_like_to_be_pwned'; then
+    bashio::config.require.safe_password 'password'
 fi
 
-# Check SSL requirements, if enabled
-if hass.config.true 'ssl'; then
-    if ! hass.config.has_value 'certfile'; then
-        hass.die 'SSL is enabled, but no certfile was specified'
-    fi
-
-    if ! hass.config.has_value 'keyfile'; then
-        hass.die 'SSL is enabled, but no keyfile was specified'
-    fi
-
-    if ! hass.file_exists "/ssl/$(hass.config.get 'certfile')"; then
-        hass.die 'The configured certfile is not found'
-    fi
-
-    if ! hass.file_exists "/ssl/$(hass.config.get 'keyfile')"; then
-        hass.die 'The configured keyfile is not found'
-    fi
-fi
+# Check SSL cerrificate
+bashio::config.require.ssl 'ssl' 'certfile' 'keyfile'
 
 # Check if database file exist
-if ! hass.file_exists "/config/$(hass.config.get 'database_path')"; then
-    hass.die 'The configured database file is not found'
+if ! bashio::fs.file_exists "/config/$(bashio::config 'database_path')"; then
+    bashio::exit.nok 'The configured database file is not found'
 fi
--- sqlite-web/rootfs/etc/cont-init.d/30-nginx.sh
+++ sqlite-web/rootfs/etc/cont-init.d/30-nginx.sh
@@ -1,37 +1,35 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: SQLite Web
 # Configures NGINX for use with SQLite Web
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
 
 declare certfile
 declare keyfile
 
 # Enable SSL
-if hass.config.true 'ssl'; then
+if bashio::config.true 'ssl'; then
     rm /etc/nginx/nginx.conf
     mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
 
-    certfile=$(hass.config.get 'certfile')
-    keyfile=$(hass.config.get 'keyfile')
+    certfile=$(bashio::config 'certfile')
+    keyfile=$(bashio::config 'keyfile')
 
     sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
     sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
 fi
 
 # Disables IPv6 in case its disabled by the user
-if ! hass.config.true 'ipv6'; then
+if ! bashio::config.true 'ipv6'; then
     sed -i '/listen \[::\].*/ d' /etc/nginx/nginx.conf
 fi
 
 # Handles the HTTP auth part
-if ! hass.config.has_value 'username'; then
-    hass.log.warning "Username/password protection is disabled!"
+if ! bashio::config.has_value 'username'; then
+    bashio::log.warning "Username/password protection is disabled!"
     sed -i '/auth_basic.*/d' /etc/nginx/nginx.conf
 else
-    username=$(hass.config.get 'username')
-    password=$(hass.config.get 'password')
+    username=$(bashio::config 'username')
+    password=$(bashio::config 'password')
     htpasswd -bc /etc/nginx/.htpasswd "${username}" "${password}"
 fi
