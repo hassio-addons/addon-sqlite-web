beb51e9b50f07d48aabb5f1f47abe1100ad524a1
Franck Nijhof <git@frenck.dev>
2021-02-02 12:55:53 +0100

    Remove stale check

--- sqlite-web/rootfs/etc/cont-init.d/sqlite-web.sh
+++ sqlite-web/rootfs/etc/cont-init.d/sqlite-web.sh
@@ -4,11 +4,6 @@
 # This files adds some patches to the add-on
 # ==============================================================================
 
-# Check if database file exist
-if ! bashio::fs.file_exists "$folder_path$(bashio::config 'database_path')"; then
-    bashio::exit.nok 'The configured database file is not found'
-fi
-
 # Adds buymeacoffe link
 patch \
     /usr/lib/python3.8/site-packages/sqlite_web/templates/base_tables.html \
