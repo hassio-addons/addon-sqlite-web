8c4e64fc0c035b560b578fa9e5f50eb8b141ca01
Franck Nijhof <frenck@addons.community>
2021-02-02 13:29:18 +0100

    Fix larger table widths on larger screens (#116)

--- sqlite-web/rootfs/etc/cont-init.d/sqlite-web.sh
+++ sqlite-web/rootfs/etc/cont-init.d/sqlite-web.sh
@@ -8,3 +8,7 @@
 patch \
     /usr/lib/python3.8/site-packages/sqlite_web/templates/base_tables.html \
     /patches/buymeacoffee
+
+# Made tables go wide!
+sed -i "s#container#container-fluid#g" \
+    /usr/lib/python3.8/site-packages/sqlite_web/templates/base.html
