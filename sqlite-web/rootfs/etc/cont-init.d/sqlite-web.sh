dfc50558884a593c6a559e7b31d7c9a27025c442
Franck Nijhof <git@frenck.dev>
2022-05-17 10:06:51 +0200

    Add optional configuration for database location (#207)

--- sqlite-web/rootfs/etc/cont-init.d/sqlite-web.sh
+++ sqlite-web/rootfs/etc/cont-init.d/sqlite-web.sh
@@ -3,6 +3,16 @@
 # Home Assistant Community Add-on: SQLite Web
 # This files adds some patches to the add-on
 # ==============================================================================
+declare database
+
+database="/config/home-assistant_v2.db"
+if bashio::config.has_value 'database'; then
+    database="$(bashio::config 'database')"
+fi
+
+if ! bashio::fs.file_exists "${database}"; then
+    bashio::exit.nok "The database file '${database}' is not found"
+fi
 
 # Adds buymeacoffe link
 patch \
